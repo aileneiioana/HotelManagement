49eb607aba6a8f8479e611f13f4202d88ab9b0ad
Ioana Ailenei <ailenei.ioana@yahoo.com>
2022-11-19 17:51:41 +0200

    Added Staff Form and Functionality

--- HotelManagement/HotelManagement/Properties/Resources.Designer.cs
+++ HotelManagement/HotelManagement/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace HotelManagement.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap back {
+            get {
+                object obj = ResourceManager.GetObject("back", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap hotel_icon {
             get {
                 object obj = ResourceManager.GetObject("hotel icon", resourceCulture);
--- HotelManagement/HotelManagement/Services/StaffServices.cs
+++ HotelManagement/HotelManagement/Services/StaffServices.cs
@@ -4,6 +4,9 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+
 
 namespace HotelManagement
 {
