8d7b7cdefad0fa20ae5af45b0870a3472a777a09
Ioana Ailenei <ailenei.ioana@yahoo.com>
2023-01-14 22:49:47 +0200

    Added Unit Test Project

---
+++ HotelManagement/UnitTestProject1/IStaff.cs
@@ -0,0 +1,9 @@
+﻿namespace UnitTestProject1
+{
+    internal interface IStaff
+    {
+        object StaffId { get; }
+        object Staffpassword { get; }
+        object Staffname { get; }
+    }
+}
